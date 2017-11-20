012ad028dbd2db008315c3cd8224934761ac75e4
Roger Le <roger.le@austin.utexas.edu>
2017-11-20 09:46:01 -0600

    Init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+app.listen(port);
+console.log('The magic happens on ' + port);
